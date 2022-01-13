89cd6098eba378fb113c20ab9d07825d4e68e36d
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-13 15:53:39 -0300

    Add test to get breeder route

--- src/tests/integration/search.test.ts
+++ src/tests/integration/search.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { breederFactory } from '@cig-platform/factories'
+import { IBreeder, IPoultry } from '@cig-platform/types'
 
 import App from '@Configs/server'
 import SearchAggregator from '@Aggregators/SearchAggregator'
@@ -8,7 +9,7 @@ describe('Search actions', () => {
   it('can search breeders', async () => {
     const breeders = Array(10).fill(breederFactory()).map((breeder) => {
       delete breeder['description']
-            
+
       return {
         ...breeder,
         foundationDate: breeder.foundationDate.toString()
@@ -26,5 +27,30 @@ describe('Search actions', () => {
       breeders
     })
     expect(mockGetBreeders).toHaveBeenCalledTimes(1)
+  })
+
+  it('can find breeder', async () => {
+    const breeder = {
+      ...breederFactory(),
+      foundationDate: new Date().toString() as any
+    } as Partial<IBreeder>
+
+    delete breeder['description']
+    
+    const poultries = [] as IPoultry[]
+
+    const mockGetBreeder = jest.fn().mockResolvedValue({ breeder, poultries })
+
+    jest.spyOn(SearchAggregator, 'getBreeder').mockImplementation(mockGetBreeder)
+
+    const response = await request(App).get(`/v1/breeders/${breeder.id}`)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toMatchObject({
+      ok: true,
+      breeder,
+      poultries
+    })
+    expect(mockGetBreeder).toHaveBeenCalledWith(breeder.id)
   })
 })
