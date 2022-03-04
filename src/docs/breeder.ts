f0a245b9b76017188fc5c1a8a6e931ceb176cd28
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-04 19:19:07 -0300

    Fix docs

--- src/docs/breeder.ts
+++ src/docs/breeder.ts
@@ -23,11 +23,13 @@ const breederDocs = {
       method: 'get',
       title: 'Get breeder poultries',
       description: 'Route to get breeder poultries',
+      queryParams: [
+        { type: 'string', name: 'pagination' },
+      ]
     }
   ], {
     pathVariables: [
       { type: 'string', name: 'breederId' },
-      { type: 'string', name: 'pagination' }
     ]
   })
 }
