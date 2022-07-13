a688586036a40ab1a033ec96fed6d930c7dd556c
edumoreira1506 <contato@eduardoem.com.br>
2022-07-13 20:40:08 -0300

    Add reviews to get breeder

---
+++ src/clients/ReviewServiceClient.ts
@@ -0,0 +1,5 @@
+import ReviewServiceClient from '@cig-platform/review-service-client'
+
+import { REVIEW_SERVICE_URL } from '@Constants/url'
+
+export default new ReviewServiceClient(REVIEW_SERVICE_URL)
