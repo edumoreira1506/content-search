8029ebb57b2e1e871aeaf24131d5ecc75a2e8c9a
edumoreira1506 <contato@eduardoem.com.br>
2022-09-10 16:31:55 -0300

    Add api-keys

--- src/clients/AccountServiceClient.ts
+++ src/clients/AccountServiceClient.ts
@@ -1,5 +1,6 @@
 import { AccountServiceClient } from '@cig-platform/core'
+import { ACCOUNT_SERVICE_API_KEY } from '@Constants/api-keys'
 
 import { ACCOUNT_SERVICE_URL } from '@Constants/url'
 
-export default new AccountServiceClient(ACCOUNT_SERVICE_URL)
+export default new AccountServiceClient(ACCOUNT_SERVICE_URL, ACCOUNT_SERVICE_API_KEY)
